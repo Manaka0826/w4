b9e3a50dee9911c576fb4253eee2c3bfb8bde6e7
Manaka0826 <144974885+Manaka0826@users.noreply.github.com>
2023-09-28 11:45:49 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -9,7 +9,7 @@ function randomValueFromArray(array){
 
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertY = ['home', 'DisneySea', 'America'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
@@ -28,7 +28,7 @@ function result() {
 
   if (customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob', name);
+    newStory = newStory.replace('Manaka', name);
   }
 
   if (document.getElementById("uk").checked) {
